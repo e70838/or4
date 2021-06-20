d60d9393c7bd631e303e1aa97f8ea676b39cf44d
jef <e70838@gmail.com>
2021-06-20 15:08:38 +0200

    before 10 juin

--- app.ts
+++ app.ts
@@ -1,5 +1,21 @@
+import fs from "fs";
+
 console.log('Hello world !');
 
-let a = 12;
+// process devient visible si on a fait : npm i --save-dev @types/node
+let cwd : string|undefined = process.env.PWD;
 
-console.log('Hello world 2 !');
+//let a:number = 12;
+
+
+console.log(process.argv);
+
+const texte:string = fs.readFileSync("toto.txt", {"encoding":"utf8"});
+
+console.log(texte);
+
+const a = {"hjk" : 12};
+a.hjk++;
+
+console.log(`Hello world from ${cwd} !`);
+
